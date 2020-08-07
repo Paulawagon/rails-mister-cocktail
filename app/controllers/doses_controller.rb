c69f3148c2d65a5eb7e05c50b1dac023fe6812f4
Paula Pisa <pisa.paula@gmail.com>
2020-08-07 17:21:32 +0200

    Cocktails unfinished

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,45 +1,24 @@
 class DosesController < ApplicationController
-  #
-
-     def index
-       @doses = Dose.all
-     end
-
-     def show
-     end
-
-     def new
-       @dose = Dose.new
-     end
-
-     def create
-       @dose = Dose.create(dose_params)
-       @cocktail = Cocktail.find(params[:cocktail_id].to_i)
-       @dose.cocktail = @cocktail
-       if @dose.save
-         redirect_to cocktail_path(@dose.cocktail)
-       else
-         # params[:dose][:cocktail_id] = @cocktail.id.to_s
-         @ingredient = Ingredient.find(params[:dose][:ingredient_id].to_i)
-         render cocktail_path(@cocktail)
-       end
-     end
-
-    def edit
+ def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
     end
+  end
 
-    def destroy
-      @dose.destroy
-      redirect_to doses_path
-    end
-    
-     private
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
 
-     def dose_params
-       params.require(:dose).permit(:cocktail_id, :ingredient_id, :description)
-     end
+  private
 
-     def set_dose
-       @dose = Dose.find(params[:id])
-     end
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,14 @@ Ingredient.create(name: "mint leaves")
 Ingredient.create(name: "Light rum")
 Ingredient.create(name: "weet Vermouth")
 Ingredient.create(name: "Strawberry schnapps")
+require 'open-uri'
+
+
+
+puts "Create ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
